6a6181e2e9aea65dd5b82220c1338572f967bfeb
copyrip <martin@copyright.rip>
2024-03-11 13:11:21 +0100

    adding css

--- site/sendmail.php
+++ site/sendmail.php
@@ -4,6 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Astroturf : Message envoyé !</title>
+<link rel="stylesheet" src="./css/reset.css">
+<link rel="stylesheet" src="./css/style.css">
 </head>
 <body>
   <div class="video-container">
